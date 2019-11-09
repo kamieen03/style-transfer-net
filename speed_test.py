5d1d68932c2a1080493b6124789cbadc2dbd94f3
Kamil Burdziński <kamil.burdzinski@student.put.poznan.pl>
2019-11-09 15:18:55 +0100

    Final speed_test

--- speed_test.py
+++ speed_test.py
@@ -13,6 +13,7 @@ from libs.utils import makeVideo
 import torch.backends.cudnn as cudnn
 from libs.models import encoder3,encoder4
 from libs.models import decoder3,decoder4
+from libs import Transfer
 import torchvision.transforms as transforms
 
 parser = argparse.ArgumentParser()
@@ -52,8 +53,8 @@ for param in matrix.parameters():
     param.requires_grad = False
 
 ################# GLOBAL VARIABLE #################
-content = torch.Tensor(1,3,800,600)
-style = torch.Tensor(1,3,800,600)
+content = torch.Tensor(1,3,576,1024)
+style = torch.Tensor(1,3,256,256)
 
 ################# GPU  #################
 if(opt.cuda):
@@ -64,13 +65,13 @@ if(opt.cuda):
     style = style.cuda()
     content = content.cuda()
 
-cap = cv2.VideoCapture('/home/kamil/Desktop/style-transfer-net/data/videos/in/in_vid.mp4')   #assume it's 800x600 (HxW)
+cap = cv2.VideoCapture('data/videos/tram.avi')   #assume it's 576x1024 (HxW)
 #cap.set(3,600)
 #cap.set(4,800)
 fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-out = cv2.VideoWriter('/home/kamil/Desktop/style-transfer-net/data/videos/out/out_vid.avi', fourcc, 20.0, (600,800))
+out = cv2.VideoWriter('data/videos/out_vid.avi', fourcc, 20.0, (1024,576))
 
-style_tmp = cv2.imread('/home/kamil/Desktop/style-transfer-net/data/style/style1.jpg')
+style_tmp = cv2.imread('data/style/27.jpg')
 style_tmp = style_tmp.transpose((2,0,1))
 style_tmp = torch.from_numpy(style_tmp).unsqueeze(0)
 style.data.copy_(style_tmp)
@@ -83,7 +84,9 @@ tt = time.time()
 while(True):
     ret, frame = cap.read()
     if not ret: break
+    print(frame.shape)
     frame = frame.transpose((2,0,1))
+    print(frame.shape)
     frame = torch.from_numpy(frame).unsqueeze(0)
     content.data.copy_(frame)
     content = content/255.0
@@ -92,18 +95,18 @@ while(True):
         cF = vgg(content)
         print(cF.shape, sF.shape)
         if(opt.layer == 'r41'):
-            feature,transmatrix = matrix(cF[opt.layer],sF[opt.layer])
+            feature = matrix(cF[opt.layer],sF[opt.layer])
         else:
-            feature,transmatrix = matrix(cF,sF)
+            feature = matrix(cF,sF)
         transfer = dec(feature)
-    transfer = transfer.clamp(0,1).squeeze(0)*255
-    transfer = transfer.type(torch.uint8).data.cpu().numpy()
-    transfer = transfer.transpose((1,2,0))
+        transfer = transfer.clamp(0,1).squeeze(0)*255
+        transfer = transfer.type(torch.uint8).data.cpu().numpy()
+        transfer = transfer.transpose((1,2,0))
 
-    #out.write(np.uint8(transfer*255))
-    #cv2.imshow('frame',transfer)
-    #if cv2.waitKey(1) & 0xFF == ord('q'):
-    #    break
+        out.write(transfer)
+        cv2.imshow('frame',transfer)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
     i += 1
     print((time.time()-tt)/i)
 
