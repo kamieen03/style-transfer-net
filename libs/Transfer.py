1a55cf0ee15b36ccbcc8d25288c91d560552dc58
Kamil Burdziński <kamil.burdzinski@student.put.poznan.pl>
2019-11-10 17:07:17 +0100

    Add transfer model using 4th layer

--- libs/Transfer.py
+++ libs/Transfer.py
@@ -1,6 +1,6 @@
 from libs.Matrix import MulLayer
-from libs.models import encoder3
-from libs.models import decoder3
+from libs.models import encoder3, encoder4
+from libs.models import decoder3, decoder4
 import torch.nn
 
 
@@ -21,4 +21,20 @@ class Transfer_r3(torch.nn.Module):
         cF = self.dec(cF)
         return cF
 
+class Transfer_r4(torch.nn.Module):
+    def __init__(self):
+        super(Transfer, self).__init__()
+        self.vgg = encoder4()
+        self.matrix = MulLayer(layer='r41')
+        self.dec = decoder4()
+        self.sF = None
+
+
+    def forward(self, content, style=None): 
+        cF = self.vgg(content)
+        if style is not None:
+            self.sF = self.vgg(style)
+        cF = self.matrix(cF, self.sF)
+        cF = self.dec(cF)
+        return cF
 
