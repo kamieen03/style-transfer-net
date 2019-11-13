e6bf588d1c2f19802d4b1d0836fc44ec7272ca0a
Kamil Burdziński <kamil.burdzinski@student.put.poznan.pl>
2019-11-13 15:00:38 +0100

    Set opset version 11

--- utils/compile_full_model.py
+++ utils/compile_full_model.py
@@ -3,8 +3,8 @@ import sys
 import os
 sys.path.append(os.path.abspath(__file__ + "/../../"))
 
-SAVE_PTH = True
-SAVE_ONNX = False
+SAVE_PTH = False
+SAVE_ONNX = True
 
 from libs.Transfer import Transfer
 from libs.models import encoder3 
@@ -17,12 +17,11 @@ model.dec.load_state_dict(torch.load('models/dec_r31.pth'))
 model.matrix.load_state_dict(torch.load('models/r31.pth'))
 model.cuda().eval()
 
-x = torch.ones(1,3,576,1024).cuda()
-y = torch.ones(1,3,576,1024).cuda()
 if SAVE_PTH:
     torch.save(model.state_dict(), 'models/transfer_r31.pth')
     print("Saved pth model")
 if SAVE_ONNX:
+    x = torch.ones(2,3,576,1024).cuda()
     torch.onnx.export(model, x, 'models/onnx/transfer_r31.onnx', verbose=True,
-                        input_names=['input_0'], output_names=['output_0'])
+                        input_names=['input_0'], output_names=['output_0'], opset_version=11)
     print("Saved onnx model")
