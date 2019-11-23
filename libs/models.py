97c78567fa98567653969b1503d9dfd8bfbeff39
Kamil Burdziński <kamil.burdzinski@student.put.poznan.pl>
2019-11-23 21:39:54 +0100

    Change raflectionPadding to zeroPadding (trt can only work with zero padding)

--- libs/models.py
+++ libs/models.py
@@ -7,14 +7,16 @@ class encoder3(nn.Module):
         # vgg
         # 224 x 224
         self.conv1 = nn.Conv2d(3,3,1,1,0)
-        self.reflecPad1 = nn.ReflectionPad2d((1,1,1,1))
+        #self.reflecPad1 = nn.ReflectionPad2d((1,1,1,1))
+        self.zeroPad1 = nn.ZeroPad2d(1)
         # 226 x 226
 
         self.conv2 = nn.Conv2d(3,64,3,1,0)
         self.relu2 = nn.ReLU(inplace=True)
         # 224 x 224
 
-        self.reflecPad3 = nn.ReflectionPad2d((1,1,1,1))
+        #self.reflecPad3 = nn.ReflectionPad2d((1,1,1,1))
+        self.zeroPad3 = nn.ZeroPad2d(1)
         self.conv3 = nn.Conv2d(64,64,3,1,0)
         self.relu3 = nn.ReLU(inplace=True)
         # 224 x 224
@@ -22,12 +24,14 @@ class encoder3(nn.Module):
         self.maxPool = nn.MaxPool2d(kernel_size=2,stride=2,return_indices = False)
         # 112 x 112
 
-        self.reflecPad4 = nn.ReflectionPad2d((1,1,1,1))
+        #self.reflecPad4 = nn.ReflectionPad2d((1,1,1,1))
+        self.zeroPad4 = nn.ZeroPad2d(1)
         self.conv4 = nn.Conv2d(64,128,3,1,0)
         self.relu4 = nn.ReLU(inplace=True)
         # 112 x 112
 
-        self.reflecPad5 = nn.ReflectionPad2d((1,1,1,1))
+        #self.reflecPad5 = nn.ReflectionPad2d((1,1,1,1))
+        self.zeroPad5 = nn.ZeroPad2d(1)
         self.conv5 = nn.Conv2d(128,128,3,1,0)
         self.relu5 = nn.ReLU(inplace=True)
         # 112 x 112
@@ -35,27 +39,28 @@ class encoder3(nn.Module):
         self.maxPool2 = nn.MaxPool2d(kernel_size=2,stride=2,return_indices = False)
         # 56 x 56
 
-        self.reflecPad6 = nn.ReflectionPad2d((1,1,1,1))
+        #self.reflecPad6 = nn.ReflectionPad2d((1,1,1,1))
+        self.zeroPad6 = nn.ZeroPad2d(1)
         self.conv6 = nn.Conv2d(128,256,3,1,0)
         self.relu6 = nn.ReLU(inplace=True)
         # 56 x 56
     def forward(self,x):
         out = self.conv1(x)
-        out = self.reflecPad1(out)
+        out = self.zeroPad1(out)
         out = self.conv2(out)
         out = self.relu2(out)
-        out = self.reflecPad3(out)
+        out = self.zeroPad3(out)
         out = self.conv3(out)
         pool1 = self.relu3(out)
         out = self.maxPool(pool1)
-        out = self.reflecPad4(out)
+        out = self.zeroPad4(out)
         out = self.conv4(out)
         out = self.relu4(out)
-        out = self.reflecPad5(out)
+        out = self.zeroPad5(out)
         out = self.conv5(out)
         pool2 = self.relu5(out)
         out = self.maxPool2(pool2)
-        out = self.reflecPad6(out)
+        out = self.zeroPad6(out)
         out = self.conv6(out)
         out = self.relu6(out)
         return out
@@ -64,7 +69,8 @@ class decoder3(nn.Module):
     def __init__(self):
         super(decoder3,self).__init__()
         # decoder
-        self.reflecPad7 = nn.ReflectionPad2d((1,1,1,1))
+        #self.reflecPad7 = nn.ReflectionPad2d((1,1,1,1))
+        self.zeroPad7 = nn.ZeroPad2d(1)
         self.conv7 = nn.Conv2d(256,128,3,1,0)
         self.relu7 = nn.ReLU(inplace=True)
         # 56 x 56
@@ -72,42 +78,46 @@ class decoder3(nn.Module):
         self.unpool = nn.UpsamplingNearest2d(scale_factor=2)
         # 112 x 112
 
-        self.reflecPad8 = nn.ReflectionPad2d((1,1,1,1))
+        #self.reflecPad8 = nn.ReflectionPad2d((1,1,1,1))
+        self.zeroPad8 = nn.ZeroPad2d(1)
         self.conv8 = nn.Conv2d(128,128,3,1,0)
         self.relu8 = nn.ReLU(inplace=True)
         # 112 x 112
 
-        self.reflecPad9 = nn.ReflectionPad2d((1,1,1,1))
+        #self.reflecPad9 = nn.ReflectionPad2d((1,1,1,1))
+        self.zeroPad9 = nn.ZeroPad2d(1)
         self.conv9 = nn.Conv2d(128,64,3,1,0)
         self.relu9 = nn.ReLU(inplace=True)
 
         self.unpool2 = nn.UpsamplingNearest2d(scale_factor=2)
         # 224 x 224
 
-        self.reflecPad10 = nn.ReflectionPad2d((1,1,1,1))
+        #self.reflecPad10 = nn.ReflectionPad2d((1,1,1,1))
+        self.zeroPad10 = nn.ZeroPad2d(1)
         self.conv10 = nn.Conv2d(64,64,3,1,0)
         self.relu10 = nn.ReLU(inplace=True)
 
-        self.reflecPad11 = nn.ReflectionPad2d((1,1,1,1))
+        #self.reflecPad11 = nn.ReflectionPad2d((1,1,1,1))
+        self.zeroPad11 = nn.ZeroPad2d(1)
         self.conv11 = nn.Conv2d(64,3,3,1,0)
 
     def forward(self,x):
         output = {}
-        out = self.reflecPad7(x)
+        out = self.zeroPad7(x)
         out = self.conv7(out)
         out = self.relu7(out)
         out = self.unpool(out)
-        out = self.reflecPad8(out)
+        out = self.zeroPad8(out)
         out = self.conv8(out)
         out = self.relu8(out)
-        out = self.reflecPad9(out)
+        out = self.zeroPad9(out)
         out = self.conv9(out)
         out_relu9 = self.relu9(out)
         out = self.unpool2(out_relu9)
-        out = self.reflecPad10(out)
+        out = self.zeroPad10(out)
         out = self.conv10(out)
         out = self.relu10(out)
-        out = self.reflecPad11(out)
+        out = self.zeroPad11(out)
         out = self.conv11(out)
         return out
 
