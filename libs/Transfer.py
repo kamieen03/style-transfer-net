69ffb0c8026f545df7c14da1e396fc5426d3fbce
Kamil Burdziński <kamil.burdzinski@student.put.poznan.pl>
2019-12-13 15:39:02 +0100

    Update pruning libs

--- libs/Transfer.py
+++ libs/Transfer.py
@@ -1,39 +1,20 @@
-from libs.Matrix import MulLayer
-from libs.models import encoder3, encoder4
-from libs.models import decoder3, decoder4
+from libs.parametric_models import encoder3, decoder3, MulLayer
 import torch.nn
 
 
 class Transfer3(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, WIDTH):
         super(Transfer3, self).__init__()
-        self.vgg_c = encoder3()
-        self.vgg_s = encoder3()
-        self.matrix = MulLayer(layer='r31')
-        self.dec = decoder3()
+        self.vgg_c = encoder3(WIDTH)
+        self.vgg_s = encoder3(WIDTH)
+        self.matrix = MulLayer(layer='r31', WIDTH)
+        self.dec = decoder3(WIDTH)
 
     def forward(self, inp): 
         # assuming inp is 5D tensor (B,6,H,W)
         cF = self.vgg_c(inp[:,:3,:,:])
         sF = self.vgg_s(inp[:,3:,:,:])
         cF = self.matrix(cF, sF)
-        cF = self.dec(cF)
-        return cF
-
-class Transfer4(torch.nn.Module):
-    def __init__(self):
-        super(Transfer4, self).__init__()
-        self.vgg = encoder4()
-        self.matrix = MulLayer(layer='r41')
-        self.dec = decoder4()
-        self.sF = None
-
-
-    def forward(self, content, style=None): 
-        cF = self.vgg(content)
-        if style is not None:
-            self.sF = self.vgg(style)
-        cF = self.matrix(cF, self.sF)
         cF = self.dec(cF)
         return cF
 
