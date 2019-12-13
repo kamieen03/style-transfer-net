22662c8bc0255d09c8b17cdc1315365999512d9b
Kamil Burdziński <kamil.burdzinski@student.put.poznan.pl>
2019-12-13 17:19:23 +0100

    Turn off gradient calculation in frozen modules

--- prune/compressor.py
+++ prune/compressor.py
@@ -41,17 +41,28 @@ class Compressor(object):
             datapath+'mscoco/validate', datapath+'wikiart/validate')
 
         # set up model and loss network
-        self.model = Transfer3(WIDTH)
+        self.model = Transfer3()
         self.model.vgg_c.load_state_dict(torch.load(vgg_path))
         self.model.vgg_s.load_state_dict(torch.load(vgg_path))
         self.model.matrix.load_state_dict(torch.load(matrix_path))
         self.model.dec.load_state_dict(torch.load(decoder_path))
         self.model.train()
         self.model.cuda()
+
         self.loss_module = encoder5()
         self.loss_module.eval()
         self.loss_module.cuda()
         self.loss_module.load_state_dict(torch.load(loss_module_path))
+
+        for param in self.model.vgg_c.parameters():
+            param.requires_grad = False
+        for param in self.model.vgg_s.parameters():
+            param.requires_grad = False
+        for param in self.model.dec.parameters():
+            param.requires_grad = False
+        for param in self.loss_module.parameters():
+            param.requires_grad = False
+
 
         # set up loss function and optimizer
         self.criterion = LossCriterion(style_layers = ['r11','r21','r31', 'r41'],
