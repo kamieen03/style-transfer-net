d6d2db5a299e3df6eaf19f2280bd4d2428d1699a
Kamil Burdziński <kamil.burdzinski@student.put.poznan.pl>
2019-12-14 14:38:37 +0100

    Update opencv build

--- utils/install_opencv.sh
+++ utils/install_opencv.sh
@@ -11,8 +11,10 @@ git checkout 3.4.2
 cd ..
 mkdir build
 cd build
-cmake -D WITH_CUDA=ON -D CUDA_GENERATION="Pascal" --D CMAKE_BUILD_TYPE=RELEASE \
+cmake -D WITH_CUDA=ON -D CUDA_GENERATION="Maxwell" --D CMAKE_BUILD_TYPE=RELEASE \
            -D WITH_CUBLAS=1 -D ENABLE_FAST_MATH=1 -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules ../opencv
 make -j $(nproc)
 make install
+cd /tmp
+rm -rf ocv_tmp
 
