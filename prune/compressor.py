e33d4f1976f652d233f613832efe8536767242f5
Kamil Burdziński <kamil.burdzinski@student.put.poznan.pl>
2019-12-15 13:44:09 +0100

    Update models

--- prune/compressor.py
+++ prune/compressor.py
@@ -10,6 +10,7 @@ import torch.optim as optim
 import copy
 import distiller
 import cv2
+import numpy as np
 
 from libs.Transfer import Transfer3
 from libs.models import encoder5
@@ -17,7 +18,7 @@ from libs.Loader import Dataset
 from libs.Criterion import LossCriterion
 
 BATCH_SIZE = 8 
-CROP_SIZE = 300
+CROP_SIZE = 256
 VGG_C_SAVE_PATH = 'models/pruned/vgg_c_r31.pth'
 VGG_S_SAVE_PATH = 'models/pruned/vgg_s_r31.pth'
 MATRIX_SAVE_PATH = 'models/pruned/matrix_r31.pth'
@@ -76,27 +77,30 @@ class Compressor(object):
         content_loader = torch.utils.data.DataLoader(dataset     = content_dataset,
                                                      batch_size  = BATCH_SIZE,
                                                      shuffle     = True,
-                                                     num_workers = 8,
+                                                     num_workers = 4,
                                                      drop_last   = True)
         style_dataset = Dataset(style_path, CROP_SIZE) 
         style_loader = torch.utils.data.DataLoader(dataset     = style_dataset,
                                                    batch_size  = BATCH_SIZE,
                                                    shuffle     = True,
-                                                   num_workers = 8,
+                                                   num_workers = 4,
                                                    drop_last   = True)
         return content_loader, style_loader
 
     def train(self):
+        best_val = 1e9
         with open('log_prune.txt', 'w+') as f:
             for epoch in range(1, EPOCHS+1): # count from one
                 self.compression_scheduler.on_epoch_begin(epoch)
                 self.train_single_epoch(epoch, f)
-                self.validate_single_epoch(epoch, f)
+                val = self.validate_single_epoch(epoch, f)
                 self.compression_scheduler.on_epoch_end(epoch)
-                torch.save(self.model.vgg_c.state_dict(), VGG_C_SAVE_PATH)
-                torch.save(self.model.vgg_s.state_dict(), VGG_S_SAVE_PATH)
-                torch.save(self.model.matrix.state_dict(), MATRIX_SAVE_PATH)
-                torch.save(self.model.dec.state_dict(), DECODER_SAVE_PATH)
+                if epoch >= 12 and val < best_val:
+                    best_val = val
+                    torch.save(self.model.vgg_c.state_dict(), VGG_C_SAVE_PATH)
+                    torch.save(self.model.vgg_s.state_dict(), VGG_S_SAVE_PATH)
+                    torch.save(self.model.matrix.state_dict(), MATRIX_SAVE_PATH)
+                    torch.save(self.model.dec.state_dict(), DECODER_SAVE_PATH)
 
     def train_single_epoch(self, epoch, f):
         batch_num = len(self.content_train)      # number of batches in training epoch
@@ -114,6 +118,8 @@ class Compressor(object):
             cF_loss = self.loss_module(content)
             tF = self.loss_module(transfer)
             loss, style_loss, content_loss = self.criterion(tF,sF_loss,cF_loss)
+            if loss.item() > 20:
+                continue
             self.compression_scheduler.before_backward_pass(epoch, batch_i, batch_num, loss, self.optimizer)
             loss.backward()
             self.compression_scheduler.before_parameter_optimization(epoch, batch_i, batch_num, self.optimizer)
@@ -131,6 +137,7 @@ class Compressor(object):
         self.model.eval()
         self.optimizer.zero_grad()
 
+        losses = []
         with torch.no_grad():
             for batch_i, (content, style) in enumerate(zip(self.content_valid, self.style_valid)):
                 content, style = content[0].cuda(), style[0].cuda()
@@ -140,6 +147,7 @@ class Compressor(object):
                 cF_loss = self.loss_module(content)
                 tF = self.loss_module(transfer)
                 loss, style_loss, content_loss = self.criterion(tF,sF_loss,cF_loss)
+                losses.append(loss.item())
 
                 print(f'Validate Epoch: [{epoch}/{EPOCHS}] ' + 
                       f'Batch: [{batch_i+1}/{batch_num}] ' +
@@ -147,6 +155,8 @@ class Compressor(object):
                 f.write(f'Validate Epoch: [{epoch}/{EPOCHS}] ' + 
                         f'Batch: [{batch_i+1}/{batch_num}] ' +
                         f'Loss: {loss:.6f} contentLoss: {content_loss:.6f} styleLoss: {style_loss:.6f}\n')
+        f.write(f'mean validation loss: {np.mean(np.array(losses))}')
+        return np.mean(np.array(losses))
 
 
 def main():
