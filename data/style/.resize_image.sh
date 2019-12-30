cb34994046329f32ba9982ddd95ace90e2f496e0
Kamil Burdziński <kamil.burdzinski@student.put.poznan.pl>
2019-12-30 12:42:28 +0100

    Remove images, logs and unused models

--- data/style/.resize_image.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-#$1 - input image
-#$2 - output image
-#$3 - output height
-#$4 - output width
-convert-im6 $1 -resample $4x$3 -resize $4x$3 $2
--- data/videos/.resize_video.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-#$1 - input video
-#$2 - output video
-#$3 - output height
-#$4 - output width
-ffmpeg -i $1 -vf scale=$4x$3 $2
