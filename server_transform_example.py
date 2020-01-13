0ae4aec7c233d9e921c8393504b52e20611da49a
Kamil <kamil.burdzinski@student.put.poznan.pl>
2020-01-13 21:05:59 +0000

    Update trt models for container; add test data

--- server_transform_example.py
+++ server_transform_example.py
@@ -2,9 +2,9 @@ from PIL import Image
 from StyleTransfer import StyleTransfer
 import numpy as np
 
-im = Image.open("data/content/057725.jpg")
+im = Image.open("data/content/1.jpg")
 im = np.asarray(im.resize((576, 1024))).transpose(2,0,1)
-style = Image.open("data/style/2.jpg")
+style = Image.open("data/style/1.jpg")
 style = np.asarray(style.resize((576, 1024))).transpose(2,0,1)
 s = StyleTransfer()
 print(im.shape, style.shape)
